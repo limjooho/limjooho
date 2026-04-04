1c3cf10c81c06368b40bfde1679127dc82f9b81d
limjooho <limjooho2690@gmail.com>
2026-04-04 21:10:04 +0900

    Update 설탕 배달.c

--- C99/백준/Silver/2839. 설탕 배달/설탕 배달.c
+++ C99/백준/Silver/2839. 설탕 배달/설탕 배달.c
@@ -3,10 +3,8 @@
 
 
 int main() {
-
 	int n;
 	scanf("%d", &n);
-
 	int answer = 0;
 
 	while (n > 0) {
@@ -27,6 +25,48 @@ int main() {
 		}
 	}
 	printf("%d", answer);
-
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ai를 이용하여 코드를 간결하게 만들어보았습니다
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h> 
+
+int main() {
+    int n;
+    if (scanf("%d", &n) == 0) return 0; // 입력 예외 처리
+
+    int answer = 0;
+
+    while (n >= 0) {
+        // 1. 현재 남은 n이 5로 나누어떨어지면, 남은 건 전부 5kg 봉지에 담는다.
+        if (n % 5 == 0) {
+            answer += (n / 5);
+            printf("%d", answer);
+            return 0; // 정답 출력 후 프로그램 즉시 종료
+        }
+
+        // 2. 5로 안 나누어떨어지면 3kg 봉지를 하나 쓴다.
+        n -= 3;
+        answer++;
+    }
+
+    // 3. while문을 빠져나왔다는 건 n이 음수가 됐다는 뜻 (정확히 못 만듦)
+    printf("-1");
+    return 0;
+}
+
+*/
